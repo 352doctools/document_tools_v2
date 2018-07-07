8dfb23d5fa46e741376153be7c5991de487ce13a
woyichengmo <864155147@.qq.com>
2018-07-08 05:28:27 +0800

    Merge branch 'master' of https://github.com/352doctools/document_tools
    # Conflicts:
    #	app/main_test.py

--- app/model/user_model.py
+++ app/model/user_model.py
@@ -1,13 +1,10 @@
-from werkzeug.security import check_password_hash
+# _*_ coding:utf-8 _*_
+
 from flask_login import UserMixin
-import json
-import uuid
-from ..auth import hash
+
 
 # define profile.json constant, the file is used to
 #  save user name and password_hash
-PROFILE_FILE = "profiles.json"
-
 
 class User(UserMixin):
     def __init__(self, uid=None, uname=None, usergroup=None):
