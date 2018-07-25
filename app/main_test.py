586fdbbdf4e4940d6d4c28e441cc395418f7e8d8
woyichengmo <864155147@.qq.com>
2018-07-25 17:55:02 +0800

    asdf

--- app/main_test.py
+++ app/main_test.py
@@ -9,7 +9,7 @@ from __init__ import create_app
 
 if __name__ == '__main__':
     # 局域网访问调试
-    create_app().run(host='127.0.0.1', port=8090, debug=True)
+    create_app().run(host='0.0.0.0', port=8090, debug=True)
     # 本机调试
     # app.run(debug=True)
 
